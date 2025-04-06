271c3f0c672246d51090c6ab42ff23c84ec127ce
manishkumarajiva <manishkumarajiva@gmail.com>
2025-04-06 16:31:22 +0530

    nest config

---
+++ dist/modules/auth/auth.module.d.ts
@@ -0,0 +1,7 @@
+import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
+export declare class AuthModule {
+    imports: [];
+    controllers: [AuthController];
+    provider: [AuthService];
+}
